a41877e467ee41a45499990ff8947d039ad2b128
Reto <reto271@gmx.ch>
2020-09-28 13:31:32 +0200

    Add help

--- DoorStateUpdater.py
+++ DoorStateUpdater.py
@@ -7,14 +7,30 @@ import telepot
 from gpiozero import Button
 from time import sleep
 
+# Print version and infos at startup
+def versionAndUsage(bot, chatId):
+    print('Door State Updater')
+    print('V01.01 B01')
+    print('')
+    print('Send the following messages to the bot:')
+    print('   T: to get the current time.')
+    print('   R: to register yourself. You will get state updates.')
+    print('   D: poll the current door state.')
+    print('   H: print this help.')
+    print('')
+    bot.sendMessage(chat_id, 'Door State Updater\n' + str(versionNumber) +
+                    '\nSend the following messages to the bot:\n' +
+                    '   T: to get the current time.\n' +
+                    '   R: to register yourself. You will get state updates.\n' +
+                    '   D: poll the current door state\n.' +
+                    '   H: print this help.\n')
 
 # Message handler for the bot
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
 
-    print 'Got cmd: %s' % command
-
+    #print 'Got cmd: %s' % command
     if command == 'T':
         bot.sendMessage(chat_id, str(datetime.datetime.now()))
     elif command == 'D':
@@ -26,27 +42,27 @@ def handle(msg):
             print('Door closed')
             bot.sendMessage(chat_id, 'Door state: closed')
     elif command == 'H':
-        print('Print help')
-        bot.sendMessage(chat_id, 'Door state (c) by Reto')
+        versionAndUsage(bot, chat_id)
     elif command == 'R':
         print('Register')
-        with open('./id.txt', 'w') as f:
+        with open('./registeredIds.txt', 'w') as f:
             f.write(str(msg['chat']['id']))
         bot.sendMessage(msg['chat']['id'],"Id saved")
 
 
 # Periodcally polls the inputs and sends status updates
 def pollInputs(button):
-
+    # Digitizes the IO states, not realy necessary...
     if button.is_pressed:
         gpioInput = 1
     else:
         gpioInput = 0
 
+    # Is there any state change? If yes and there are registered users, send thy update.
     if gpioInput != pollInputs.oldGpioInput:
         print 'Gpio changed to: ' + str(gpioInput)
         try:
-            with open('./id.txt', 'r') as idfile:
+            with open('./registeredIds.txt', 'r') as idfile:
                 chat_id=int(idfile.read())
                 if pollInputs.oldGpioInput == 2:
                     print('-> Booted')
@@ -76,6 +92,7 @@ def readTelegramId():
 
 # Main program
 pollInputs.oldGpioInput = 2
+versionAndUsage()
 myTelegramId = readTelegramId()
 if '' != myId:
     bot = telepot.Bot(myTelegramId)
