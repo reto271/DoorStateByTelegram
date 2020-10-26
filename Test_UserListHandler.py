88319ae73876141558a1c320be62a05d1dad88f6
reto271 <reto271@gmx.ch>
2020-10-26 21:33:52 +0100

    First unit test added

---
+++ Test_UserListHandler.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import unittest
+import os
+from UserListHandler import UserListHandler
+
+class Test_UserListHandler(unittest.TestCase):
+    def __initializeTest(self):
+        if os.path.exists('./testIds.txt'):
+            os.remove('./testIds.txt')
+
+    def test_isListEmpty(self):
+        self.__initializeTest()
+        m_userList = UserListHandler(False)
+        m_userList.initialize('./testIds.txt')
+        m_userList.loadList()
+        self.assertEqual(False, m_userList.isListEmpty())
+
+
+unittest.main()  # Calling from the command line invokes all tests
